1612832e6f4fe6bbea3ace1b8b8387e296456834
kaurr <kaurr@DESKTOP-O8AIF6C>
2023-01-20 20:47:44 -0700

    New Project Called Fun Facts Created a short list of facts.

---
+++ src/fun/facts/FunFacts.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fun.facts;
+
+/**
+ *
+ * @author kaurr
+ */
+public class FunFacts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("*****************************************************************************");
+        System.out.println("*************************** 5   F U N   F A C T S ***************************");
+        System.out.println("*****************************************************************************");
+        System.out.println(" ");
+        System.out.println("1. When you sleep, you can't smell anything.");
+        System.out.println("2. The brain uses about 10 watts of energy to help an individual think.");
+        System.out.println("3. When you're cold, the finger nails tend to grow faster.");
+        System.out.println("4. Snails can sleep for up to 3 years.");
+        System.out.println("5. A tsunami can travel as fast as a jet plane.");
+        System.out.println(" ");
+        System.out.println("**************************** R E F E R E N C E *******************************");
+        System.out.println(" ");
+        System.out.println("Rachel. (2023, January 5). 50 random facts that you won't believe are true. Kids Activities Blog. ");
+        System.out.println("Retrieved January 20, 2023, from https://kidsactivitiesblog.com/76701/50-random-facts/ ");
+        System.out.println(" ");
+
+    }
+    
+}
